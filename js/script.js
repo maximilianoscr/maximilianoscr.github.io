621c6cce9b2140e157054c0e39e59e1f2d301081
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2023-06-21 23:27:49 -0300

    pruebas

--- js/script.js
+++ js/script.js
@@ -20,5 +20,12 @@ document.addEventListener('DOMContentLoaded', function() {
     resetNavbarOpacity();
   });
   resetNavbarOpacity();
-
+  function myFunction() {
+    var x = document.getElementById("menu_bar");
+    if (x.className === "") {
+      x.className += " responsive";
+    } else {
+      x.className = "";
+    }
+  }
 });
