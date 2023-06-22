4a913ea91e2204028370f197ab9a52f32cb7942a
Maxi <maximiliano.scromeda@riesgonet.com>
2023-06-22 10:18:44 -0300

    cambios

--- js/script.js
+++ js/script.js
@@ -20,12 +20,13 @@ document.addEventListener('DOMContentLoaded', function() {
     resetNavbarOpacity();
   });
   resetNavbarOpacity();
-  function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className === "") {
-      x.className += " responsive";
-    } else {
-      x.className = "";
-    }
-  }
+
 });
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "") {
+    x.className += " responsive";
+  } else {
+    x.className = "";
+  }
+}
