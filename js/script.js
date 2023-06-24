c373736abf45d851e322d6fea26832eddb7bd8c8
Maxi <maximiliano.scromeda@riesgonet.com>
2023-06-24 16:24:40 -0300

    subo cambios

--- js/script.js
+++ js/script.js
@@ -1,26 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var navbar = document.getElementById('navbar');
-  var timer;
-  function setNavbarOpacity(opacity) {
-    navbar.style.opacity = opacity;
-  }
-  function resetNavbarOpacity() {
-    clearTimeout(timer);
-    setNavbarOpacity(1);
-    timer = setTimeout(function() {
-      setNavbarOpacity(0.1);
-    }, 15000);
-  }
-  navbar.addEventListener('mouseenter', function() {
-    clearTimeout(timer);
-    setNavbarOpacity(1);
-  });
-  navbar.addEventListener('mouseleave', function() {
-    resetNavbarOpacity();
-  });
-  resetNavbarOpacity();
 
-});
 function myFunction() {
   var x = document.getElementById("navbar");
   if (x.className === "") {
